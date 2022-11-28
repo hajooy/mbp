b05ab0b3f6b42835600b50f455b07c6dedf8049c
hajooy <115938530+hajooy@users.noreply.github.com>
2022-11-29 01:39:08 +0900

    Update before_deploy.sh

--- scripts/before_deploy.sh
+++ scripts/before_deploy.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 cd /usr/local/tomcat/webapps
-mv ROOT.war appspec.yml stop_tomcat.sh start_tomcat.sh before_deploy.sh /backup/
-mv /usr/local/tomcat/webapps/ROOT/ROOT.war /usr/local/tomcat/webapps/
-rm -rf /usr/local/tomcat/webapps/ROOT
+rm -rf scripts/
