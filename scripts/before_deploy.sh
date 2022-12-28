90825c727b380aecef769dfcced15e880b1bf277
hajooy <115938530+hajooy@users.noreply.github.com>
2022-12-29 01:58:52 +0900

    Delete scripts directory

--- scripts/before_deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd /backup
-rm -rf ROOT.war scripts appspec.yml
-cd /usr/local/tomcat/webapps
-mv -f appspec.yml scripts ROOT.war /backup/
--- scripts/start_tomcat.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash  
-cd /usr/local/tomcat/bin/
-./startup.sh
